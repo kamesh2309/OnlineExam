187cca5afb14c22da4613598a04589abfecc7293
unknown <kkamesh2309@gmail.com>
2023-12-06 23:09:49 +0530

    Login page done updated

---
+++ online-exam/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from "./components/Header"
+import Login from './components/Login'
+import Footer from './components/Footer'
+const App = () => {
+  return (
+    <div className='container-fluid'>
+      <Header/>
+      <Login/>
+      <Footer/>
+      </div>
+  )
+}
+export default App
